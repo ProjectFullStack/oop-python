3f5662170ee1a7b8a97f4ec1d701974aeee07a44
jon <jon@glidepathsolutions.com>
2021-09-18 08:35:04 -0400

    adding more print statements to student.py

--- 3_class_and_instance_variables/student.py
+++ 3_class_and_instance_variables/student.py
@@ -25,18 +25,22 @@ class Student:
 
 joe = Student('Joe', 18, 8429, ["MATH401", "COMP505"])
 print("Created Joe!")
+print("joe.student_count = ", joe.student_count)
 print("Total student count is now: ", Student.student_count)
 
 jane = Student('Jane', 20, 2931, ["HIST420", "COMP505"])
+print('-------')
 print("Created Jane!")
+print("joe.student_count = ", joe.student_count)
+print("jane.student_count = ", jane.student_count)
 print("Total student count is now: ", Student.student_count)
-
+#
+#
 Student.student_count += 1
+print('-------')
 print("Manually changed student count!")
+print("joe.student_count = ", joe.student_count)
+print("jane.student_count = ", jane.student_count)
 print("Total student count is now: ", Student.student_count)
-
-print(jane.student_count)
-jane.student_count += 1
-print(jane.student_count)
 
 
